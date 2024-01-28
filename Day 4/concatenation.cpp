1a94a0c558c5b50d5f70768c5b5d0f95b8ff0ed6
gracygulati7 <124993594+gracygulati7@users.noreply.github.com>
2024-01-28 18:54:54 +0530

    Add files via upload

---
+++ Day 4/concatenation.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+class Solution {
+public:
+    vector<int> getConcatenation(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> ans(2 * n);
+        for (int i = 0; i < n; i++) {
+            ans[i] = nums[i];
+            ans[i + n] = nums[i];
+        }
+        return ans;
+    }
+};
+int main() {
+    cout << "Enter the number of elements in the vector: ";
+    int size;
+    cin >> size;
+    vector<int> nums(size);
+    cout << "Enter the elements of the vector, separated by space:" << endl;
+    for (int i = 0; i < size; i++) {
+        cin >> nums[i];
+    }
+    Solution solution;
+    vector<int> concatenated = solution.getConcatenation(nums);
+    cout << "Concatenated Vector: ";
+    for (int i = 0; i < concatenated.size(); i++) {
+        cout << concatenated[i] << " ";
+    }
+    return 0;
+}
---
+++ Day 4/ispowerof2.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+class Solution {
+public:
+    bool isPowerOfTwo(int x) {
+        if(x > 0) {
+            if ((x & (x - 1)) == 0) {
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            return false;
+        }
+    }
+};
+int main() {
+    Solution solution;
+    int x;
+    cout << "Enter a number: ";
+    cin >> x;
+    if(solution.isPowerOfTwo(x)) {
+        cout << "true" << endl;
+    } else {
+        cout << "false" << endl;
+    }
+    return 0;
+}
