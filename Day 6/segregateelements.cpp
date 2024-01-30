c83483e8e7fc3eada8361c35dd84c336091c7c9d
gracygulati7 <124993594+gracygulati7@users.noreply.github.com>
2024-01-30 20:20:47 +0530

    Add files via upload

---
+++ Day 6/segregateelements.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+void segregateElements(int arr[],int n){
+        vector<int> result;
+        for(int i=0;i<n;i++){
+            if(arr[i]>0){
+                result.push_back(arr[i]);
+            }
+        }
+        for(int i=0;i<n;i++){
+            if(arr[i]<0){
+                result.push_back(arr[i]);
+            }
+        }
+        for(int i=0;i<n;i++){
+            arr[i]=result[i];
+        }
+}
+int main() {
+    int n;
+    cout << "Enter the size of the array: ";
+    cin >> n;
+    int arr[n];
+    cout << "Enter " << n << " elements:\n";
+    for(int i = 0; i < n; i++)
+        cin >> arr[i];
+    segregateElements(arr, n);
+    cout << "Array after segregating positive and negative elements: ";
+    for(int i = 0; i < n; i++)
+        cout << arr[i] << " ";
+    cout << endl;
+    return 0;
+}
