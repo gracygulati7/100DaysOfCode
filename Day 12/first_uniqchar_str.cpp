3d65c41ccf542b395b4e1b8201d11386217fe781
gracygulati7 <124993594+gracygulati7@users.noreply.github.com>
2024-02-05 21:00:48 +0530

    Add files via upload

---
+++ Day 12/first_uniqchar_str.cpp
@@ -0,0 +1,39 @@
+//! find the first unique character in a string
+
+#include <iostream>
+#include <string>
+using namespace std;
+class Solution {
+public:
+    int firstUniqChar(string s) {
+        int n = s.length();
+        for (int i = 0; i < n; i++) {
+            bool isUnique = true;
+            for (int j = 0; j < n; j++) {
+                if (i != j && s[i] == s[j]) {
+                    isUnique = false;
+                    break;
+                }
+            }
+            if (isUnique) {
+                return i;
+            }
+        }
+        return -1;
+    }
+};
+int main() {
+    Solution solution;
+    string input;
+    cout << "Enter a string: ";
+    getline(cin, input);
+    int result = solution.firstUniqChar(input);
+    if (result != -1) {
+        cout << "The first non-repeating character is at index: " << result << endl;
+    } else {
+        cout << "No unique character found in the string." << endl;
+    }
+    return 0;
+}
+
+// https://leetcode.com/problems/first-unique-character-in-a-string/description/?envType=daily-question&envId=2024-02-05
