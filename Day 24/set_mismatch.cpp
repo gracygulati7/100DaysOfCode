1032a276aa0aeeaf7c88cc4e2293e90a7abac6c0
gracygulati7 <124993594+gracygulati7@users.noreply.github.com>
2024-02-17 21:51:03 +0530

    Add files via upload

---
+++ Day 24/set_mismatch.cpp
@@ -0,0 +1,30 @@
+//! leetcode question 645. Set Mismatch
+
+//? link: https://leetcode.com/problems/set-mismatch/description/?envType=daily-question&envId=2024-02-17
+
+#include <vector>
+#include <algorithm>
+class Solution {
+public:
+    std::vector<int> findErrorNums(std::vector<int>& nums) {
+        int n = nums.size();
+        std::vector<int> res;
+        sort(nums.begin(), nums.end());
+        for (int i = 0; i < n - 1; i++) {
+            if (nums[i] == nums[i + 1]) {
+                // Found the duplicate
+                res.push_back(nums[i]);
+                // Find the missing number
+                for (int j = 1; j <= n; j++) {
+                    if (std::find(nums.begin(), nums.end(), j) == nums.end()) {
+                        res.push_back(j);
+                        break;
+                    }
+                }
+                return res;
+            }
+        }
+        // If no duplicate is found, something unexpected happened
+        return res;
+    }
+};
