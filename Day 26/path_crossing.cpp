0dffda41dbbde6d75c82abd0790875af2864842b
gracygulati7 <124993594+gracygulati7@users.noreply.github.com>
2024-02-19 20:17:17 +0530

    Add files via upload

---
+++ Day 26/path_crossing.cpp
@@ -0,0 +1,30 @@
+//! leetcode question 1496. Path Crossing
+
+//? link: https://leetcode.com/problems/path-crossing/description/?envType=daily-question&envId=2024-02-19
+
+#include <string>
+#include <set>
+#include <vector>
+using namespace std;
+class Solution {
+public:
+    bool isPathCrossing(string path) {
+        int n = path.size();
+        int x = 0, y = 0;
+        set<vector<int>> paths;
+        paths.insert({0, 0});
+        for (char i : path) {
+            switch (i) {
+                case 'N': y++; break;
+                case 'S': y--; break;
+                case 'E': x++; break;
+                case 'W': x--; break;
+            }
+            if (paths.find({x, y}) != paths.end()) {
+                return true;
+            }
+            paths.insert({x, y});
+        }
+        return false;
+    }
+};
