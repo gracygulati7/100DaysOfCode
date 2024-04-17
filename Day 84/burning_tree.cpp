e3e98d0def0e47795cfa0ffab9c05717edf26ef7
Gracy <gracy331.be22@chitkara.edu.in>
2024-04-17 14:01:20 +0530

    🤍

--- Day 84/burning_tree.cpp
+++ Day 84/burning_tree.cpp
@@ -21,7 +21,7 @@ public:
         if (root == NULL) { // This condition checks if the current node root is NULL (if we have reached a leaf node or a non-existent node). 
         // If so, it updates the ans variable with the maximum distance encountered so far.
             ans = max(ans, distance - 1);
-            return 0;
+            return 0; // This line returns 0, indicating that the target node has not been found in this subtree
         }
         if (root->data == target || distance > 0) { // This condition checks if the current node's data matches the target node's value target,
         // or if the distance from the root is greater than 0 (indicating that the target node has been found in a previous recursive call). 
@@ -29,15 +29,15 @@ public:
         
             find(root->left, target, distance + 1);
             find(root->right, target, distance + 1);
-            return distance - 1;
+            return distance - 1; // this line returns the distance from the root to the current node minus 1 
         }
         else { // This block executes when the current node is not the target node, and its distance from the root is 0.
             int left = find(root->left, target, distance); // This line recursively calls find on the left child of the current node.
             int right = find(root->right, target, distance); // This line recursively calls find on the right child of the current node.
 
             if (left < 0) { // This condition checks if the distance returned from the left child is negative 
-            // (which indicates that the target node is found in the left subtree).
-            // If so, it updates ans and performs a new recursive call to handle the right subtree.
+            // (which indicates that the target node is found in the left subtree). If so, it updates ans with 
+            // the absolute value of left and performs a new recursive call to handle the right subtree with adjusted distance (1 - left).
                 ans = max(ans, -left);
                 find(root->right, target, 1 - left);
                 return left - 1;
@@ -54,7 +54,8 @@ public:
         // and neither its left nor right child contains the target node. It returns 0 to indicate that 
         // the target node has not been found in the current subtree.
     }
-    int minTime(Node *root, int target) {
+    int minTime(Node *root, int target) { // This function is the entry point for calculating the minimum time to burn the tree. 
+    // It calls the find function with the initial distance as 0 and returns the final result stored in ans.
         find(root, target, 0);
         return ans;
     }
